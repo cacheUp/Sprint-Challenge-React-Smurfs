1a928091930caa8c5b0c79e41ce6231a7205945c
cacheUp <bradb26@gmail.com>
2019-01-25 20:58:34 +0300

    added a delete button

--- village/src/components/Smurf.js
+++ village/src/components/Smurf.js
@@ -1,20 +1,31 @@
-import React from 'react';
+import React, { useContext } from "react";
+import { SmurfPostContext } from "../providers/SmurfPostProvider";
+import axios from "axios";
 
 const Smurf = props => {
+  const { smurfData, setSmurfData } = useContext(SmurfPostContext);
+  const baseUrl = "http://localhost:3333";
+
+  const deleteSmurf = (e, id) => {
+    e.preventDefault();
+    axios
+      .delete(`${baseUrl}/smurfs/${id}`)
+      .then(res => {
+        setSmurfData(res.data);
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  };
+
   return (
     <div className="Smurf">
       <h3>{props.name}</h3>
       <strong>{props.height} tall</strong>
       <p>{props.age} smurf years old</p>
+      <button onClick={e => deleteSmurf(e, props.id)}>Delete</button>
     </div>
   );
 };
 
-Smurf.defaultProps = {
-  name: '',
-  height: '',
-  age: ''
-};
-
 export default Smurf;
-
